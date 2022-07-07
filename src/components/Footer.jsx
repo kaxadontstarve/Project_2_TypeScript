933e7a466323038d171192616f1f870eba05bd0d
kaxadontstarve <kaxa0308super@gmail.com>
2022-07-07 16:56:22 +0300

    completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,13 +17,10 @@ const Footer = () => {
             <FooterColumn title='Исполнителю' items={[' Начать работу', 'Заказы', 'Условия и цены']} />
             <FooterColumn title='Компания' items={[' О проекте', 'Поддержка', 'Сообщество']} />
             <Stack className={cl.footer__column}>
-                <Typography
-                    sx={{
-                        fontSize: '20px',
-                        fontWeight: '500',
-                    }}
-                    className={cl.footer__title}>
-                    Мобильные приложения
+                <Typography>
+                    <div className={cl.footer__title}>
+                        Мобильные приложения
+                    </div>
                 </Typography>
                 <Stack direction='row' className={cl.footer__row}>
                     <Stack className={cl.footer__items}>
